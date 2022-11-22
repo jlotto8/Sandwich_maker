ff5a4475dc4a6870e08243ac167bfbc3484765f7
jlotto8 <jlotto8@gmail.com>
2022-11-22 18:10:24 -0500

    updated so all the buttons work

--- sandwich_stuff/sandwich_app/urls.py
+++ sandwich_stuff/sandwich_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from sandwich_app.views import SandwichappView, IngredientsView, SampleSandwichView, EntireMenuView
+from sandwich_app.views import SandwichappView, IngredientsView, SandwichGeneratorView, MenuView
 
 urlpatterns = [
-    path('', SandwichappView.as_view(), name='sandwich_home'),
+    path('', SandwichappView.as_view(), name='sandwich'),
     path('ingredients/<str:ingredient_type>', IngredientsView.as_view(), name='ingredients_list'),
-    path('sample/', SampleSandwichView.as_view(), name='sample_sandwich'),
-    path('menu/', EntireMenuView.as_view(), name='entire_sandwich_menu'),
+    path('random', SandwichGeneratorView.as_view(), name='sandwich_generator'),
+    path('menu/', MenuView.as_view(), name='menu'),
 ]
--- sandwich_stuff/sandwich_app/views.py
+++ sandwich_stuff/sandwich_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from  django.views import View
-
+from django.http import Http404
 import random
-
 
 ingredients = {
     'meats': ['turkey', 'salami', 'pepperoni', 'sliced chicken', 'pastrami', 'tuna', 'veggie burger', 'ham'],
     'cheeses': ['provolone', 'american', 'cheddar', 'swiss', 'pepper jack', 'fondue'],
     'toppings': ['lettuce', 'tomato', 'banana peppers', 'hot peppers', 'onions', 'olives', 'spinach', 'avocado', 'cucumber', 'cream cheese', 'relish']
     }
+
 
 class SandwichappView(View):
     def get(self, request):
@@ -20,6 +20,9 @@ class SandwichappView(View):
 
 class IngredientsView(View):
     def get(self, request, ingredient_type):
+        if ingredient_type not in ingredients:
+            raise Http404(f'No such ingredient: {ingredient_type}')
+
         return render(
             request = request,
             template_name = 'ingredients_list.html',
@@ -27,28 +30,32 @@ class IngredientsView(View):
                 'ingredients': ingredients[ingredient_type],
                 'ingredient_type': ingredient_type
             }
-        ),
+        )
 
-class SampleSandwichView(View):
+class SandwichGeneratorView(View):
     def get(self, request):
-        return render (
-            request = request,
-            template_name = 'sample_sandwich.html',
-            context = {
-                'meat': random.choice(ingredients['meats']),
-                'cheese': random.choice(ingredients['cheeses']),
-                'topping': random.choice(ingredients['toppings']),
-            }
-        ),
+          selected_meat = random.choice(ingredients['meats'])
+          selected_cheese = random.choice(ingredients['cheeses'])
+          selected_toppings = random.choice(ingredients['toppings'])
+          sandwich = f"{selected_meat} and {selected_cheese} with {selected_toppings}"
+          return render(
+            request, "sandwich_generator.html", context={'sandwich': sandwich}
+          )
+      
 
-class EntireMenuView(View):
+class MenuView(View):
     def get(self, request):
+        menus = []
+        for meat in ingredients['meats']:
+            for cheese in ingredients['cheeses']:
+                for topping in ingredients ['toppings']:
+                    sandwich = f'{meat} & {cheese} with {topping}'
+                    menus.append(sandwich)
+        print(menus)
         return render (
-            request = request,
-            template_name = 'entire_sandwich_menu.html',
+            request,
+           'menu.html',
             context = {
-                'meats': ingredients['meats'],
-                'cheeses': ingredients['cheeses'],
-                'toppings': ingredients['toppings'],
+                'menus' : menus
             }
         )
